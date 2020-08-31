946334e38dc4ec7b6ebd8af998cd8d5293ff2d2c
Alok_singh <ALOK743165@GMAIL.COM>
2020-08-31 16:25:09 +0530

    basic C++tamplet

---
+++ cp.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fo(i, n) for (i = 0; i < n; i++)
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define PI 3.1415926535897932384626
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+     srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+ 
+    while( t-- ) {
+ 
+    }
+ 
+    return 0;
+}
